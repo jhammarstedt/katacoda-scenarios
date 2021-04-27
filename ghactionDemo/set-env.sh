0d2fb3471f570bfd42e06c1f4e368db2892e26b2
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2021-04-27 09:59:05 +0200

    Create set-env.sh

---
+++ ghactionDemo/set-env.sh
@@ -0,0 +1,12 @@
+echo "Cloning repo Benchmarking-DevOps"
+git clone https://github.com/jhammarstedt/Benchmarking-DevOps.git
+
+echo "Giving chmod access to all relevant scripts"
+chmod +x scripts/clear_tutorial.sh scripts/clear_table.sh scripts/collectEgg.sh
+
+echo "Setting up cool easter egg"
+
+# Configuring a BOT github account
+echo "Config github BOT credentials"
+git config --global user.email "you@example.com"
+git config --global user.name "Tutorial bot"
